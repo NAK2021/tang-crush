d419d793bd7d94ede846c24ba30f4b0681627c56
NAK2021 <87328801+NAK2021@users.noreply.github.com>
2021-07-12 23:30:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trả lời điều này thật lòng nhá
+    Thích anh honggg <3 <3`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Cho anh 1 lý do được hong nè',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Thích thì thích hoiiii ',
+    mess: 'Anh biết mà 🥰.Anh cũng vậy nè 😘😘',
+    messDesc: 'Hi vọng em thích món quà nhỏ này :>>',
+    btnAccept: 'Hihihi <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100036082902119' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
